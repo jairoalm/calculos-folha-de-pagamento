a276bb3a220479fefcd256a1fee06ffab24435e3
Jairo Almeida <jairoam85@gmail.com>
2021-07-03 19:26:54 -0300

    Implementando a lógica do calculo do decimo terceiro

--- src/br/com/jairo/model/DecimoTerceiro.java
+++ src/br/com/jairo/model/DecimoTerceiro.java
@@ -1,5 +1,28 @@
 package br.com.jairo.model;
 
+import java.util.Scanner;
+
 public class DecimoTerceiro {
+	private static Scanner s;
+	
+	public static void main (String [] args ){
+		
+		s = new Scanner (System.in);
+		
+		double salario = 0;
+		double mestrabalhado = 0;
+		double resultado;
+		
+		System.out.println("Informe o valor do salário : ");
+		salario = s.nextInt();
+		
+	    System.out.println("Mês trabalhado : ");
+	    mestrabalhado = s.nextInt();
+	    
+	    resultado = salario / 12 * mestrabalhado;
+	    
+	    System.out.println(resultado);
+	}
+	
 
 }
